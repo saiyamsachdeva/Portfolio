d7b1ba651574afa37073bfacb9f11063298aa1a8
Saiyam Sacheva <94432762+saiyamsachdeva@users.noreply.github.com>
2023-12-11 00:59:04 +0530

    3rd commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function downloadCV() {
+    window.open('https://drive.google.com/file/d/1Dny081Mcb2v5CpZ8ktSDByOcG-15eye5/view?usp=sharing', '_blank');
+    // var link = document.createElement('a');
+    //     link.href = 'https://drive.google.com/file/d/1Dny081Mcb2v5CpZ8ktSDByOcG-15eye5/view?usp=sharing';
+    //     link.download = 'https://drive.google.com/file/d/1Dny081Mcb2v5CpZ8ktSDByOcG-15eye5/view?usp=sharing'; // You can set the desired file name here
+    //     document.body.appendChild(link);
+    //     link.click();
+    //     document.body.removeChild(link);
+}
+
+function openLinkedIN() {
+    window.open('https://www.linkedin.com/in/saiyam-sachdeva-a79099216/', '_blank');
+}
+
+function openchatBotAppGitHubRepo() {
+    window.open('https://github.com/saiyamsachdeva/chat_bot_app','_blank')
+}
+function openchatAppGitHubRepo(){
+    window.open('https://github.com/saiyamsachdeva/Chat_App','_blank')
+}
