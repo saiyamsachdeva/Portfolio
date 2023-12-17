e1a9cd93bdeadf5e720066beef4ca5d8c15c0f48
Saiyam Sacheva <94432762+saiyamsachdeva@users.noreply.github.com>
2023-12-18 01:10:46 +0530

    3rd commit

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 function downloadCV() {
     window.open('https://drive.google.com/file/d/1Dny081Mcb2v5CpZ8ktSDByOcG-15eye5/view?usp=sharing', '_blank');
-    // var link = document.createElement('a');
-    //     link.href = 'https://drive.google.com/file/d/1Dny081Mcb2v5CpZ8ktSDByOcG-15eye5/view?usp=sharing';
-    //     link.download = 'https://drive.google.com/file/d/1Dny081Mcb2v5CpZ8ktSDByOcG-15eye5/view?usp=sharing'; // You can set the desired file name here
-    //     document.body.appendChild(link);
-    //     link.click();
-    //     document.body.removeChild(link);
+    
 }
 
 function openLinkedIN() {
@@ -15,6 +10,23 @@ function openLinkedIN() {
 function openchatBotAppGitHubRepo() {
     window.open('https://github.com/saiyamsachdeva/chat_bot_app','_blank')
 }
+function openchatBotAppLiveLink(){
+    window.open('https://scintillating-lokum-5c8971.netlify.app/#/','_blank')
+}
+
 function openchatAppGitHubRepo(){
     window.open('https://github.com/saiyamsachdeva/Chat_App','_blank')
 }
+
+function openchatAppLiveLink() {
+    window.open('https://whimsical-puppy-224d6f.netlify.app/','_blank')
+}
+
+function openbiteGitHubRepo() {
+    window.open('https://github.com/saiyamsachdeva/food_ecommerce_app','_blank')
+}
+
+function openbiteLiveLink() {
+    window.open('https://github.com/saiyamsachdeva/food_ecommerce_app','_blank')
+}
+
